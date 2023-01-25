b0d3e5c919398018bbc4352929394dc18e863d71
Francisco Tomás <camelocomsede1@gmail.com>
2023-01-25 11:13:11 +0000

    second step done, place order finished

--- client/src/scenes/checkout/Payment.jsx
+++ client/src/scenes/checkout/Payment.jsx
@@ -1,7 +1,40 @@
 import React from "react"
+import { Box, Typography, TextField } from "@mui/material"
 
-const Payment = () => {
-  return <div>Payment</div>
+const Payment = ({ values, errors, touched, handleBlur, handleChange }) => {
+  return (
+    <Box m="30px 0">
+      <Box>
+        <Typography sx={{ mb: "15px", fontSize: "18px" }}>
+          Contact Info
+        </Typography>
+        <TextField
+          fullWidth
+          type="text"
+          label="Email"
+          onBlur={handleBlur}
+          onChange={handleChange}
+          value={values.email}
+          name="email"
+          error={!!touched.email && !!errors.email}
+          helperText={touched.email && errors.email}
+          sx={{ gridColumn: "span 4", marginBottom: "15px" }}
+        />
+        <TextField
+          fullWidth
+          type="text"
+          label="Phone Number"
+          onBlur={handleBlur}
+          onChange={handleChange}
+          value={values.phoneNumber}
+          name="phoneNumber"
+          error={!!touched.phoneNumber && !!errors.phoneNumber}
+          helperText={touched.phoneNumber && errors.phoneNumber}
+          sx={{ gridColumn: "span 4" }}
+        />
+      </Box>
+    </Box>
+  )
 }
 
 export default Payment
